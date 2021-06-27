0161f4f18bbb62d856e0241fe10f45039cc646e4
felipe-almeida80 <felipe.almeida80@hotmail.com>
2021-06-27 11:20:55 -0300

    selectors

--- testExample.py
+++ testExample.py
@@ -17,18 +17,12 @@ driver.find_element_by_css_selector (jsonObject['selecionarCor']).click()
 sleep(5)
 driver.find_element_by_css_selector(jsonObject['selecionarCapacidade']).click()
 sleep(5)
-#Selecionando a capacidade 
-driver.find_element_by_css_selector('#Item364gb_label > span:nth-child(3) > span > span').click()
-
-sleep(3)
-driver.find_element_by_css_selector('#primary > summary-builder > div.as-purchaseinfo > div:nth-child(1) > div > div.grouped-button-icon > div:nth-child(2) > div > div > form > div > span > button > span').click()
+driver.find_element_by_css_selector(jsonObject['colocarSacola']).click()
 sleep(5)
-#sacola
-driver.find_element_by_css_selector('#root > div.rf-attach > div > div > div.rf-summary-header-right > div > form > button').click()
-
+driver.find_element_by_css_selector(jsonObject['verSacola']).click()
 #Pagar
 sleep(5)
-driver.find_element_by_css_selector('#shoppingCart\.actions\.navCheckout').click()
+driver.find_element_by_css_selector(jsonObject['pagar']).click()
 #Finalizar Convidade
 sleep(5)
 driver.find_element_by_css_selector('#signIn\.guestLogin\.guestLogin').click()
